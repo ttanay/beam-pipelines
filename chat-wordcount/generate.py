6ebbac77a8aec2f37b2bdfa2be85780d68d688b1
ttanay <ttanay100@gmail.com>
2019-05-09 18:12:30 +0530

    some progress

--- chat-wordcount/generate.py
+++ chat-wordcount/generate.py
@@ -1,5 +1,9 @@
+import re
+import pprint
+import time
+
 def generate_dictionary(filename):
-    with open(filename, 'r'):
+    with open(filename, 'r') as file:
         dictionary = {}
         pattern = r'[\d]+/[\d]+/[\d]+, [\d]+:[\d]+ - '
         current_message = ''
@@ -19,3 +23,36 @@ def generate_dictionary(filename):
 
         for key in dictionary.keys():
             dictionary[key] = ' '.join(dictionary[key])
+    return dictionary
+
+def parse_message(file):
+    RE_TIMESTAMP = r'[\d]+/[\d]+/[\d]+, [\d]+:[\d]+'
+    RE_SENDER = r'[\D]+'
+    RE_MSG = r'[\D]+'
+
+    RE_SINGLE_MSG = RE_TIMESTAMP + r' - ' + RE_SENDER + r': ' + RE_MSG
+    RE_CONTINUED_MSG = RE_MSG
+    RE_SYSTEM_MSG = RE_TIMESTAMP + r' - ' + RE_MSG
+
+    sender = ''
+    for line in file:
+        if re.match(RE_SINGLE_MSG, line):
+            sender, msg = map(str.strip, line.split('-', 1)[1].split(':', 1))
+        elif re.match(RE_CONTINUED_MSG, line):
+            msg = line
+        elif re.match(RE_SYSTEM_MSG, line):
+            _, msg = map(str.strip, line.split('-', 1))
+            sender = 'SYSTEM'
+        print(line)
+        yield (sender, msg)
+
+def dummy(file):
+    return parse_message(file)
+
+if __name__ == '__main__':
+    #pprint.pprint(generate_dictionary('../WhatsApp_Chat_with_Yashaswini.txt'))
+    file = open('../../WhatsApp_Chat_with_Yashaswini.txt', 'r')
+    for message in dummy(file):
+        print(message)
+        print('\n\n')
+        time.sleep(1)
--- chat-wordcount/most_used_words_per_person.py
+++ chat-wordcount/most_used_words_per_person.py
@@ -35,28 +35,76 @@ Output:
 
 import apache_beam as beam
 
-with beam.Pipeline(beam.PipelineOoptions()) as p:
+#with beam.Pipeline(beam.PipelineOptions()) as p:
 
+#    most_used_words_per_person = (p
+#                                  | 'ReadChatFile' >> beam.io.Read(ChatFileSource(filepath))
+#                                  | 'GroupMsgPerPerson' >> beam.GroupByKey()
+#                                  | 'JoinMsgStrings' >> beam.Map(lambda x: (x[0], ' '.join(x[1])))
+#                                  | 'SplitMsgStringToWords' >> beam.FlatMap(string_to_words)
+#                                  | 'MakeWordOnePairs' >> beam.FlatMap(lambda x: (x[0], ()))
+#                                  | 'CountWordsPerPerson' >> beam.FlatMap(count_words_in_msg))
+
+# TODO: Think of a better way to represent each sender, set of words.
+# Eg: ((sender, word), count) may be an option. Using this, it is easy
+# to count occurences per (sender, word) which is intended as we need
+# Count of words per person. So, (sender, word) pair may be a logical key.
+
+with beam.Pipeline(beam.PipelineOptions()) as p:
     most_used_words_per_person = (p
-                                  | 'ReadChatFile' >> beam.ReadChatFile(filepath)
-                                  | 'CountWordsPerPerson' >> CountWordsPerPerson()
-                                  | 'SortWordCount' >> SortWordCount())
+                                  | 'ReadChatFile' >> beam.io.Read(ChatFileSource(filepath))
+                                  | 'SplitWordsPerLine' >> beam.ParDo(split_words)
+                                  | 'MakeWordOnePairs' >> beam.ParDo(lambda x: ((x), 1))
+                                  | 'GroupByWordSenderPairs' >> beam.GroupByKey()
+                                  | 'CountWordSender' >> beam.Map(lambda x: (x[0], sum(x[1]))))
 
-
-class ReadChatFile(beam.io.filebasedsource.FileBasedSource):
+class ChatFileSource(beam.io.filebasedsource.FileBasedSource):
     """From the raw text, add transforms to seperate the strings by person"""
-    # Add the parsing logic here while reading lines from file
-    # Reference: https://stackoverflow.com/questions/41170997/how-to-convert-csv-into-a-dictionary-in-apache-beam-dataflow/41171867#41171867
-    pass
+
+    RE_TIMESTAMP = r'[\d]+/[\d]+/[\d]+, [\d]+:[\d]+ -'
+    RE_SENDER = r'[\D]+:'
+    RE_MSG = r'[\w\']+'
+
+    RE_SINGLE_MSG_LINE = RE_TIMESTAMP + RE_SENDER + RE_MSG
+    RE_CONTINUED_MSG = RE_MSG
+    RE_SYSTEM_MSG = RE_TIMESTAMP + RE_MSG
+
+    def parse_message(self, line):
+        sender = ''
+        for line in file:
+            if re.match(RE_SINGLE_MSG, line):
+                sender, msg = map(str.strip, line.split('-', 1)[1].split(':', 1))
+            elif re.match(RE_CONTINUED_MSG, line):
+                msg = line
+            elif re.match(RE_SYSTEM_MSG, line):
+                _, msg = map(str.strip, line.split('-', 1))
+                sender = 'SYSTEM'
+            yield (sender, msg)
+
+    def read_records(self, file_name, range_tracker):
+        with open(file_name, 'r') as file:
+            return parse_message(file)
 
 
-@beam.ptransform_fn
-def CountWordsPerPerson(pcoll):
-    """Count the words used be each person"""
-    pass
+def split_words(element):
+    sender = element[0]
+    msg = element[1]
+    words = re.findall(r'[\w\']+', msg)
+    return [(sender, word) for word in words]
 
 
-@beam.ptransform_fn
-def SortWordCount(pcoll):
-    """Sort the word-counts of each peron by the count in ascending order"""
-    pass
+# REMOVE
+def string_to_words(element):
+    string = element[1]
+    words = re.findall(r'[\w\']+', string)
+    return (element[0], words)
+
+
+# REMOVE
+def make_word_one_pairs(element):
+    words = element[1]
+    # TODO: Is it more efficient if the map is done
+    # with another Beam PTransform? Since each user
+    # will have loads of words.
+    word_one_pairs = map(words, lambda x: (x, 1))
+    return (element[0], word_one_pairs)
